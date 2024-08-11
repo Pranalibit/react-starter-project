240b6f22a3e9c4783ec8a42e87e8534d764f4546
prana <pranalipardeshi30@gmail.com>
2024-08-11 13:28:15 +0530

    Props concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import img from './assets/react-core-concepts.png';
+import Header from './components/Header.jsx';
+import {CORE_CONCEPTS} from './data.js';
+
+function CoreConcepts({image, title, description}) {
+    return (
+        <div>
+            <li>
+        <img src={image}/>
+        <p>{title}</p>
+        <h3>{description}</h3>
+        </li>
+        </div>
+        );
+    }
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+        <Header/>
       <main>
+          <section id="core-concepts">
+              <h2>Core-Concepts</h2>
+              <ul>
+{/*                   here props name are similar to proerty name of object then you can use spread operator to pull out key value pair from data */}
+                  <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+                  <CoreConcepts {...CORE_CONCEPTS[1]}/>
+                  <CoreConcepts {...CORE_CONCEPTS[2]}/>
+                  <CoreConcepts {...CORE_CONCEPTS[3]}/>
+                  </ul>
+              </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
